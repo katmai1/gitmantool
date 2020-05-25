094beb43a832a6df2ebce869b23669fbc93fe08b
katmai <katmai.mobil@gmail.com>
2020-05-25 14:18:20 +0200

    basic

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gitmanager',
+  packages = ['gitmanager'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Console tool for manage projects on github/gitlab',
+  author = 'katmai',
+  author_email = 'katmai.mobil@gmail.com',
+  url = 'https://github.com/katmai1/gitmanager', # use the URL to the github repo
+  download_url = 'https://github.com/katmai1/gitmanager/tarball/0.1',
+  keywords = ['git', 'github', 'gitlab', 'console'],
+  classifiers = [],
+)
